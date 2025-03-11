b51d3da6da26ffaf12c62a380a847a6c9785f031
AreaGit <areagit5@gmail.com>
2025-03-11 11:33:36 -0300

    Definindo PORT

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8008;
+const port = process.env.PORT || 8008;
 const path = require('path');
 const multer = require('multer');
 const xlsx = require('xlsx');
